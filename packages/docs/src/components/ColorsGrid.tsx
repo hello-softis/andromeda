c3947cf55ed3bc77357d85941273e8ff5b249d6d
castrogusttavo.dev <castrogusttavo.dev@gmail.com>
2024-07-17 20:02:02 +0000

    feat: add all tokens documentation

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -11,35 +11,28 @@ export function ColorsGrid() {
     return acc;
   }, {} as Record<string, string[]>);
 
-  return Object.entries(colorGroups).map(([color, keys]) => {
-    return (
-      <div key={color} style={{ backgroundColor: color, padding: '2rem' }}>
-        {keys.length === 1 ? (
-          <div style={{ 
-            display: 'flex', 
-            justifyContent: 'space-between', 
-            fontFamily: 'monospace', 
-            color: getContrast(color, '#FFFFFF') < 3.5 ? '#000000' : '#FFFFFF' 
-            }}
-          >
-            <strong>{keys[0]}</strong>
-            <span>{color}</span>
+  return (
+    <div className="grid">
+      {Object.entries(colorGroups).map(([color, keys]) => {
+        const textColor = getContrast(color, '#F5F5FA') < 3.5 ? 'text-gray-900' : 'text-gray-50';
+
+        return (
+          <div key={color} className="p-8" style={{ backgroundColor: color }}>
+            {keys.length === 1 ? (
+              <div className={`flex justify-between font-mono ${textColor}`}>
+                <strong>${keys[0]}</strong>
+                <span>{color}</span>
+              </div>
+            ) : (
+              <div className={`flex justify-between items-center font-mono ${textColor}`}>
+                <strong>${keys[0]}</strong>
+                <strong>{keys[1]}</strong>
+                <span>{color}</span>
+              </div>
+            )}
           </div>
-        ) : (
-          <div style={{ 
-            display: 'flex', 
-            justifyContent: 'space-between', 
-            alignItems: 'center',
-            fontFamily: 'monospace',
-            color: getContrast(color, '#FFFFFF') < 3.5 ? '#000000' : '#FFFFFF' 
-            }}
-          >
-            <strong>{keys[0]}</strong>
-            <strong>{keys[1]}</strong>
-            <span>{color}</span>
-          </div>
-        )}
-      </div>
-    )
-  })
+        );
+      })}
+    </div>
+  );
 }
