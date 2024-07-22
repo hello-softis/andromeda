45a82b1bf7dfd2d8ce310a22a36c2af9b43c2aab
castrogusttavo.dev <castrogusttavo.dev@gmail.com>
2024-07-22 03:50:29 +0000

    feat: add icon-button component

--- packages/docs/src/stories/button/IconButton.stories.tsx
+++ packages/docs/src/stories/button/IconButton.stories.tsx
@@ -1,10 +1,40 @@
 import '../../index.css';
 import type { StoryObj, Meta } from '@storybook/react';
-import { Box, BoxProps } from '@andromeda/react';
+import { ButtonIcon, ButtonIconProps } from '@andromeda/react';
+import { PlusIcon, GearIcon, DoubleArrowUpIcon } from '@radix-ui/react-icons'
+
+const iconMap = {
+  plus: <PlusIcon />,
+  gear: <GearIcon />,
+  doubleArrowUp: <DoubleArrowUpIcon />,
+};
 
 export default {
   title: 'Form/Button/IconButton',
-  component: Box,
+  component: ButtonIcon,
+  args: {
+    children: iconMap['plus'],
+    colors: 'primary',
+    sizes: 'md',
+    loading: false,
+    disable: false
+  },
+  argTypes: {
+    children: {
+      control: { type: 'select' },
+      options: Object.keys(iconMap),
+      mapping: iconMap,
+    },
+    colors: {
+      control: { type: 'inline-radio' },
+      options: ['primary', 'secondary', 'tertiary'],
+    },
+    sizes: {
+      control: { type: 'inline-radio' },
+      options: ['sm', 'md', 'lg'],
+    },
+    disable: { control: 'boolean' },
+  },
   decorators: [
     (Story) => (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
@@ -12,6 +42,30 @@ export default {
       </div>
     ),
   ],
-} as Meta<BoxProps>;
+} as Meta<ButtonIconProps>
 
-export const Default: StoryObj<BoxProps> = {}
+export const Primary: StoryObj<ButtonIconProps> = {}
+
+export const Secondary: StoryObj<ButtonIconProps> = {
+  args: {
+    colors: 'secondary',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonIconProps> = {
+  args: {
+    colors: 'tertiary',
+  },
+}
+
+export const Loading: StoryObj<ButtonIconProps> = {
+  args: {
+    loading: true,
+  },
+}
+
+export const Disabled: StoryObj<ButtonIconProps> = {
+  args: {
+    disable: true,
+  },
+}
