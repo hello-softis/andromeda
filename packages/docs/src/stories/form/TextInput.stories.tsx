5bdca25c114d1d4c30b8ff6dda745265a37b3be9
castrogusttavo.dev <castrogusttavo.dev@gmail.com>
2024-07-22 18:59:56 +0000

    feat: add icon left or riht in input and button component

--- packages/docs/src/stories/form/TextInput.stories.tsx
+++ packages/docs/src/stories/form/TextInput.stories.tsx
@@ -1,39 +1,52 @@
 import '../../index.css';
 import type { StoryObj, Meta } from '@storybook/react';
 import { InputText, InputTextProps, Box, Text } from '@andromeda/react';
+import { LockClosedIcon, EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons'
+
+const iconMap = {
+  noIcon: '',
+  Locked: <LockClosedIcon />,
+  EyeClose: <EyeClosedIcon />,
+  EyeOpen: <EyeOpenIcon />,
+};
 
 export default {
   title: 'Form/TextInput',
   component: InputText,
   args: {
-    placeholder: '',
+    placeholder: 'Placeholder',
     prefix: '',
-    disabled: false
+    disabled: false,
+    invalid: false,
+    leadingIcon: iconMap['noIcon'],
+    trailingIcon: iconMap['noIcon']
   },
   argTypes: {
     placeholder: { control: 'text' },
     prefix: { control: 'text' },
-    disabled: { control: 'boolean' }
+    disabled: { control: 'boolean' },
+    invalid: { control: 'boolean' },
+    leadingIcon: {
+      control: { type: 'select' },
+      options: Object.keys(iconMap),
+      mapping: iconMap,
+    },
+    trailingIcon: {
+      control: { type: 'select' },
+      options: Object.keys(iconMap),
+      mapping: iconMap,
+    },
   },
   decorators: [
     (Story) => (
       <div className="flex justify-center items-center min-h-screen">
-        <Box className='w-full max-w-full'>
-          <div className='p-3 flex flex-col gap-2'>
-            <Text sizes='sm' weight={true}>Email address</Text>
-            <Story/>
-          </div>
-        </Box>
+        <Story/>
       </div>
     ),
   ],
 } as Meta<InputTextProps>;
 
-export const Default: StoryObj<InputTextProps> = {
-  args: {
-    placeholder: 'Enter your email'
-  }
-}
+export const Default: StoryObj<InputTextProps> = {}
 
 export const Disabled: StoryObj<InputTextProps> = {
   args: {
@@ -44,5 +57,17 @@ export const Disabled: StoryObj<InputTextProps> = {
 export const WithPrefix: StoryObj<InputTextProps> = {
   args: {
     prefix: 'cal.com/'
+  }
+}
+
+export const WithIcon: StoryObj<InputTextProps> = {
+  args: {
+    leadingIcon: iconMap['Locked'],
+  }
+}
+
+export const WithError: StoryObj<InputTextProps> = {
+  args: {
+    invalid: true
   }
 }
