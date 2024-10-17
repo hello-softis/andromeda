d671e4dca9e224069b1020efb84337569966901e
castrogusttavo <gusttavoleandro2023@gmail.com>
2024-10-17 14:47:40 -0300

    feat: add skeleton component

---
+++ packages/react/src/components/skeleton.tsx
@@ -0,0 +1,11 @@
+export interface SkeletonProps {
+  className?: string
+}
+
+export function Skeleton({ className }: SkeletonProps) {
+  return (
+    <div
+      className={`bg-slate-300 dark:bg-grey-600 animate-pulse ${className}`}
+    />
+  )
+}
